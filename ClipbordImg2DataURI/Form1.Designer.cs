73f5151f9a59b93678762f4f6d6f09b4b05252f2
udaken <uda.ken+git@gmail.com>
2018-07-02 23:20:24 +0900

    プロジェクト ファイルを追加します。

---
+++ ClipbordImg2DataURI/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace ClipbordImg2DataURI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.scale100Percent = new System.Windows.Forms.RadioButton();
+            this.scale50Percent = new System.Windows.Forms.RadioButton();
+            this.scale25Percent = new System.Windows.Forms.RadioButton();
+            this.scale12Percent = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.outputFormatPng8 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.outputFormatJpeg = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(8, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.copyButton.Enabled = false;
+            this.copyButton.Location = new System.Drawing.Point(551, 734);
+            this.copyButton.Margin = new System.Windows.Forms.Padding(2);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(98, 29);
+            this.copyButton.TabIndex = 1;
+            this.copyButton.Text = "&Copy data URI";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.copyButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox1.Controls.Add(this.flowLayoutPanel1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 653);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(384, 41);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "縮小";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.scale100Percent);
+            this.flowLayoutPanel1.Controls.Add(this.scale50Percent);
+            this.flowLayoutPanel1.Controls.Add(this.scale25Percent);
+            this.flowLayoutPanel1.Controls.Add(this.scale12Percent);
+            this.flowLayoutPanel1.Controls.Add(this.checkBox1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(2, 14);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(380, 25);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // scale100Percent
+            // 
+            this.scale100Percent.AutoSize = true;
+            this.scale100Percent.Checked = true;
+            this.scale100Percent.Location = new System.Drawing.Point(2, 2);
+            this.scale100Percent.Margin = new System.Windows.Forms.Padding(2);
+            this.scale100Percent.Name = "scale100Percent";
+            this.scale100Percent.Size = new System.Drawing.Size(47, 16);
+            this.scale100Percent.TabIndex = 0;
+            this.scale100Percent.TabStop = true;
+            this.scale100Percent.Text = "100%";
+            this.scale100Percent.UseVisualStyleBackColor = true;
+            this.scale100Percent.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // scale50Percent
+            // 
+            this.scale50Percent.AutoSize = true;
+            this.scale50Percent.Location = new System.Drawing.Point(53, 2);
+            this.scale50Percent.Margin = new System.Windows.Forms.Padding(2);
+            this.scale50Percent.Name = "scale50Percent";
+            this.scale50Percent.Size = new System.Drawing.Size(41, 16);
+            this.scale50Percent.TabIndex = 1;
+            this.scale50Percent.TabStop = true;
+            this.scale50Percent.Text = "50%";
+            this.scale50Percent.UseVisualStyleBackColor = true;
+            this.scale50Percent.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // scale25Percent
+            // 
+            this.scale25Percent.AutoSize = true;
+            this.scale25Percent.Location = new System.Drawing.Point(98, 2);
+            this.scale25Percent.Margin = new System.Windows.Forms.Padding(2);
+            this.scale25Percent.Name = "scale25Percent";
+            this.scale25Percent.Size = new System.Drawing.Size(41, 16);
+            this.scale25Percent.TabIndex = 2;
+            this.scale25Percent.TabStop = true;
+            this.scale25Percent.Text = "25%";
+            this.scale25Percent.UseVisualStyleBackColor = true;
+            this.scale25Percent.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // scale12Percent
+            // 
+            this.scale12Percent.AutoSize = true;
+            this.scale12Percent.Location = new System.Drawing.Point(143, 2);
+            this.scale12Percent.Margin = new System.Windows.Forms.Padding(2);
+            this.scale12Percent.Name = "scale12Percent";
+            this.scale12Percent.Size = new System.Drawing.Size(49, 16);
+            this.scale12Percent.TabIndex = 4;
+            this.scale12Percent.TabStop = true;
+            this.scale12Percent.Text = "12.5%";
+            this.scale12Percent.UseVisualStyleBackColor = true;
+            this.scale12Percent.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(196, 2);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(123, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "シャープネスフィルター";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.Controls.Add(this.outputFormatJpeg);
+            this.groupBox2.Controls.Add(this.outputFormatPng8);
+            this.groupBox2.Location = new System.Drawing.Point(13, 698);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(375, 36);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "画像フォーマット";
+            // 
+            // outputFormatPng8
+            // 
+            this.outputFormatPng8.AutoSize = true;
+            this.outputFormatPng8.Checked = true;
+            this.outputFormatPng8.Location = new System.Drawing.Point(6, 14);
+            this.outputFormatPng8.Name = "outputFormatPng8";
+            this.outputFormatPng8.Size = new System.Drawing.Size(52, 16);
+            this.outputFormatPng8.TabIndex = 0;
+            this.outputFormatPng8.Text = "PNG8";
+            this.outputFormatPng8.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(13, 744);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(379, 19);
+            this.textBox1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.outputFormatJpeg.AutoSize = true;
+            this.outputFormatJpeg.Location = new System.Drawing.Point(161, 14);
+            this.outputFormatJpeg.Name = "radioButton1";
+            this.outputFormatJpeg.Size = new System.Drawing.Size(52, 16);
+            this.outputFormatJpeg.TabIndex = 1;
+            this.outputFormatJpeg.Text = "JPEG";
+            this.outputFormatJpeg.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 770);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ClipbordImg2DataURI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.RadioButton scale100Percent;
+        private System.Windows.Forms.RadioButton scale50Percent;
+        private System.Windows.Forms.RadioButton scale25Percent;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton outputFormatPng8;
+        private System.Windows.Forms.RadioButton scale12Percent;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton outputFormatJpeg;
+    }
+}
+
---
+++ ClipbordImg2DataURI/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImageProcessor;
+
+namespace ClipbordImg2DataURI
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32")]
+        public static extern IntPtr SetClipboardViewer(
+                IntPtr hWndNewViewer);
+
+        [DllImport("user32")]
+        public static extern bool ChangeClipboardChain(
+                IntPtr hWndRemove, IntPtr hWndNewNext);
+
+        [DllImport("user32")]
+        public extern static int SendMessage(
+                IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private const int WM_DRAWCLIPBOARD = 0x0308;
+        private const int WM_CHANGECBCHAIN = 0x030D;
+        private IntPtr nextHandle;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ClipboardHandler += Form1_ClipboardHandler;
+        }
+
+        private Image ImageFilter(Image img)
+        {
+            var size = img.Size;
+            int factor = 1;
+            if (scale50Percent.Checked)
+            {
+                factor = 2;
+            }
+            else if (scale25Percent.Checked)
+            {
+                factor = 4;
+            }
+            else if (scale12Percent.Checked)
+            {
+                factor = 8;
+            }
+            if (size.Width < factor) throw new InvalidOperationException();
+            if (size.Height < factor) throw new InvalidOperationException();
+
+            size = new Size(size.Width / factor, size.Height / factor);
+
+            using (var factory = new ImageFactory().Load(img))
+            {
+                using (var f = factory.Resize(size))
+                {
+                    return f.Image.Clone() as Image;
+                }
+            }
+        }
+
+        private Image Source;
+
+        private static void SaveDispose<T>(ref T obj) where T : IDisposable
+        {
+            if (obj != null)
+            {
+                obj.Dispose(); obj = default;
+            }
+        }
+
+        private void Form1_ClipboardHandler(object sender, ClipboardImageEventArgs ev)
+        {
+            SaveDispose(ref Source);
+            Source = ev.Image;
+            if(pictureBox1.Image != null)
+            {
+                pictureBox1.Image.Dispose();
+                pictureBox1.Image = null;
+            }
+            pictureBox1.Image = ImageFilter(Source);
+            copyButton.Enabled = true;
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            nextHandle = SetClipboardViewer(this.Handle);
+
+            base.OnHandleCreated(e);
+        }
+        protected override void OnHandleDestroyed(EventArgs e)
+        {
+            // ビューアを解除
+            bool sts = ChangeClipboardChain(this.Handle, nextHandle);
+
+            base.OnHandleDestroyed(e);
+        }
+
+        public event ClipboardHandler ClipboardHandler;
+        protected override void WndProc(ref Message msg)
+        {
+            switch (msg.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    if (Clipboard.ContainsImage())
+                    {
+                        // クリップボードの内容がテキストの場合のみ
+                        // クリップボードの内容を取得してハンドラを呼び出す
+                        ClipboardHandler?.Invoke(this, new ClipboardImageEventArgs(Clipboard.GetImage()));
+                    }
+                    if (nextHandle != IntPtr.Zero)
+                        SendMessage(
+                            nextHandle, msg.Msg, msg.WParam, msg.LParam);
+                    msg.Result = IntPtr.Zero;
+                    return;
+                // クリップボード・ビューア・チェーンが更新された
+                case WM_CHANGECBCHAIN:
+                    if (msg.WParam == nextHandle)
+                        nextHandle = msg.LParam;
+                    else if (nextHandle != IntPtr.Zero)
+                        SendMessage(nextHandle, msg.Msg, msg.WParam, msg.LParam);
+                    msg.Result = IntPtr.Zero;
+                    return;
+            }
+            base.WndProc(ref msg);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Source != null)
+                pictureBox1.Image = ImageFilter(Source);
+        }
+
+        private ImageProcessor.Imaging.Formats.ISupportedImageFormat MakeImageFormat()
+        {
+            if (outputFormatJpeg.Checked)
+            {
+                return new ImageProcessor.Imaging.Formats.JpegFormat
+                {
+                    Quality = 90
+                };
+
+            }
+            else
+            {
+                return new ImageProcessor.Imaging.Formats.PngFormat
+                {
+                    IsIndexed = true
+                };
+            }
+
+        }
+
+        private void copyButton_Click(object sender, EventArgs e)
+        {
+            if (Source != null)
+                using (var factory = new ImageFactory().Load(ImageFilter(Source)))
+                {
+                    var format = MakeImageFormat();
+                    using (var f = factory.Format(format))
+                    {
+                        using (var stream = new System.IO.MemoryStream())
+                        {
+                            f.Save(stream);
+                            var dataUri = "data:" + format.MimeType + ";base64," + Convert.ToBase64String(stream.GetBuffer(), Base64FormattingOptions.None);
+                            try
+                            {
+                                Clipboard.Clear();
+                                Clipboard.SetText(dataUri);
+
+                                textBox1.Text = $"{dataUri.Length} 文字コピーしました。";
+                            }catch(ExternalException ex)
+                            {
+                                MessageBox.Show(this, "クリップボードの設定に失敗しました。\n" + ex.Message,  this.Text,  MessageBoxButtons.OK ,MessageBoxIcon.Error);
+                            }
+                        }
+                    }
+                }
+        }
+    }
+    public delegate void ClipboardHandler(
+                            object sender, ClipboardImageEventArgs ev);
+
+    public class ClipboardImageEventArgs : EventArgs
+    {
+        public Image Image { get; }
+        public ClipboardImageEventArgs(Image str) => this.Image = str;
+    }
+}
